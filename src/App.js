f66e8c22bc3c40a5c09e9ffb86d6c1f6298fcda0
Shikha Dhiman <dshikha4403@gmail.com>
2023-01-29 22:14:27 +0530

    changed state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-import Navbar from './components/Navbar'
-import TextArea from './components/TextArea'
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextArea from "./components/TextArea";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  function handleMode() {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "khaki";
+    }
+  }
   return (
     <>
       <Navbar
@@ -12,11 +23,17 @@ function App() {
         head2="Contact"
         dropdown="Activities"
         id="body"
+        mode={mode}
       />
       <br />
       <div className="container" id="body">
         <center>
-          <TextArea title="Enter the text" id="textArea" />
+          <TextArea
+            title="Enter the text"
+            id="textArea"
+            mode={mode}
+            handleMode={handleMode}
+          />
         </center>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a
+          className={`navbar-brand text-${
+            props.mode === "light" ? "dark" : "white"
+          }`}
+          href="/"
+        >
           {props.title}
         </a>
         <button
@@ -23,23 +27,41 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a
+                className={`nav-link active text-${
+                  props.mode === "light" ? "dark" : "white"
+                }`}
+                aria-current="page"
+                href="/"
+              >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a
+                className={`nav-link text-${
+                  props.mode === "light" ? "dark" : "white"
+                }`}
+                href="/"
+              >
                 {props.head1}
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a
+                className={`nav-link text-${
+                  props.mode === "light" ? "dark" : "white"
+                }`}
+                href="/"
+              >
                 {props.head2}
               </a>
             </li>
-            <li className="nav-item dropdown">
+            <li className="nav-item dropdown ">
               <a
-                className="nav-link dropdown-toggle"
+                className={`nav-link dropdown-toggle text-${
+                  props.mode === "light" ? "dark" : "white"
+                }`}
                 href="/"
                 role="button"
                 data-bs-toggle="dropdown"
@@ -79,12 +101,12 @@ Navbar.Prototypes = {
   title: PropTypes.string.isRequired,
   head1: PropTypes.string.isRequired,
   head2: PropTypes.string.isRequired,
-  dropdown: PropTypes.string
+  dropdown: PropTypes.string,
 };
 
-Navbar.defaultProps={
-    title:"Title Here",
-    head1:"Heading1",
-    head2:"Heading2",
-    dropdown:"Dropdown"
-}
+Navbar.defaultProps = {
+  title: "Title Here",
+  head1: "Heading1",
+  head2: "Heading2",
+  dropdown: "Dropdown",
+};
