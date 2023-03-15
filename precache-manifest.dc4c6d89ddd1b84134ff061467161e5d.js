09eb745e8d1c7bd58a3550b18c303486542a017c
Shikha Dhiman <dshikha4403@gmail.com>
2023-03-15 15:36:51 +0530

    Updates

---
+++ precache-manifest.dc4c6d89ddd1b84134ff061467161e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc1932bb19476556f12e",
+    "url": "/Text-Utility/static/css/main.40ae95b5.chunk.css"
+  },
+  {
+    "revision": "cc1932bb19476556f12e",
+    "url": "/Text-Utility/static/js/main.cc1932bb.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/Text-Utility/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "c642814f8060c0c14b32",
+    "url": "/Text-Utility/static/js/2.c642814f.chunk.js"
+  },
+  {
+    "revision": "3d48cf89da3e4f59552b",
+    "url": "/Text-Utility/static/js/runtime~main.3d48cf89.js"
+  },
+  {
+    "revision": "9ccf81a3e61cc7ee91b3b563266351be",
+    "url": "/Text-Utility/index.html"
+  }
+];
